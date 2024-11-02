e95ae61b82b183a06fc344a981a730383464ff5b
Reshma <reshmashaik0910@gmail.com>
2024-11-02 17:40:26 +0000

    changed restaurant card index

--- src/hooks/useRestaurantData.js
+++ src/hooks/useRestaurantData.js
@@ -15,8 +15,8 @@ const useRestaurantData = ()=>{
             const json = await data.json();
             console.log(json);
            // const restaurants = json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-            setRestaurantList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)  
-            setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+            setRestaurantList(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants)  
+            setFilteredRestaurants(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         } catch (error) {
             console.log("Error fetching data :", error);    
         }    
